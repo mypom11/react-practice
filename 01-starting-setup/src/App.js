8d48173cbff6bca7e79740593029959bc31d5f58
jeongki Min <mypom11@gmail.com>
2023-08-29 21:32:24 +0900

    first commit

---
+++ 01-starting-setup/src/App.js
@@ -0,0 +1,12 @@
+import Expensese from './components/Expenses/Expenses'
+
+const App = () => {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expensese />
+    </div>
+  )
+}
+
+export default App
---
+++ 02-practice/src/App.js
@@ -0,0 +1,13 @@
+import ConceptComponent from './components/ConceptComponent'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <ConceptComponent />
+    </div>
+  )
+}
+
+export default App
