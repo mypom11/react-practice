aaaf247c3895ff7cd75111decc371ee1d644268e
jeongki Min <mypom11@gmail.com>
2023-08-31 04:53:18 +0900

    react Hooks

---
+++ 09-useReducer/src/App.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react'
+
+import Login from './components/Login/Login'
+import Home from './components/Home/Home'
+import MainHeader from './components/MainHeader/MainHeader'
+import AuthContext from './components/store/auth-context'
+
+function App() {
+  const ctx = useContext(AuthContext)
+  return (
+    <>
+      <MainHeader onLogout={ctx.logoutHandler} />
+      <main>
+        {!ctx.isLoggedIn && <Login onLogin={ctx.loginHandler} />}
+        {ctx.isLoggedIn && <Home onLogout={ctx.logoutHandler} />}
+      </main>{' '}
+    </>
+  )
+}
+
+export default App
